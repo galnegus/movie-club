26c2d79c7c3921c8d1635af3d56df06b1a8e49eb
galnegus <tobiashindersson@hotmail.com>
2017-04-11 16:13:29 +0200

    added login and logout

--- src/jsx/components/sidebar.jsx
+++ src/jsx/components/sidebar.jsx
@@ -1,22 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Login } from './sidebar/login.jsx';
+import Login from './sidebar/login.jsx';
 import { Watched } from './sidebar/watched.jsx';
 
 export class Sidebar extends Component {
-  constructor() {
-    super();
-    this.state = {
-        // user: null,
-         user: "admin",
-        // user: "member",
-    };
-  }
-
   render() {
   	return (
   	  <div className="sidebar">
-        <Login user={this.state.user} />
+        <Login />
         <div>
           <Watched />
         </div>
--- src/jsx/components/sidebar/login.jsx
+++ src/jsx/components/sidebar/login.jsx
@@ -1,45 +1,91 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import { connect } from 'react-redux'
+import { firebaseConnect, pathToJS } from 'react-redux-firebase'
 
-export class Login extends Component{
+class Login extends Component{
+  constructor(props) {
+    super(props);
+
+    this.login = this.login.bind(this);
+    this.setEmailRef = this.setEmailRef.bind(this);
+    this.setPasswordRef = this.setPasswordRef.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  login(e) {
+    e.preventDefault();
+
+    this.props.firebase.login({
+      email: this.emailInput.value,
+      password: this.passwordInput.value
+    });
+  }
+
+  logout(e) {
+    e.preventDefault();
+    this.props.firebase.logout();
+  }
+
+  setEmailRef(input) {
+    this.emailInput = input;
+  }
+
+  setPasswordRef(input) {
+    this.passwordInput = input;
+  }
+
   render(){
-    if(!this.props.user){
+    if (!this.props.auth) { // not logged in
       return(
         <div className='sidebar-menu'>
-          <strong className='sidebar-menu__heading'>Login</strong>
           <ul className='sidebar-menu__list'>
             <li className='sidebar-menu__item'><Link to="/">Home</Link></li>
-            <li className='sidebar-menu__item'><input type="text" placeholder="username"></input></li>
-            <li className='sidebar-menu__item'><input type="text" placeholder="password"></input></li>
-            <li className='sidebar-menu__item'><button type="submit">Login</button></li>
           </ul>
+          <form onSubmit={this.login}>
+            <input type="text" placeholder="email" ref={this.setEmailRef} />
+            <input type="password" placeholder="password" ref={this.setPasswordRef} />
+            <button type="submit">Login</button>
+          </form>
           <hr />
         </div>
       );
-    } else if(this.props.user === "admin") {
-      return(
-        <div className='sidebar-menu'>
-          <strong className='sidebar-menu__heading'>Cool Guy</strong>
-          <ul className='sidebar-menu__list'>
-            <li className='sidebar-menu__item'><Link to="/">Home</Link></li>
-            <li className='sidebar-menu__item'><Link to="/addmovie">Add movie</Link></li>
-            <li className='sidebar-menu__item'><Link to="/schedule">Schedule</Link></li>
-            <li className='sidebar-menu__item'>Logout</li>
-          </ul>
-          <hr />
-        </div>
-      );
-    } else if(this.props.user === "member") {
-      return(
-        <div className='sidebar-menu'>
-          <strong className='sidebar-menu__heading'>Cool Guy</strong>
-          <ul className='sidebar-menu__list'>
-            <li className='sidebar-menu__item'><Link to="/">Home</Link></li>
-            <li className='sidebar-menu__item'>Logout</li>
-          </ul>
-          <hr />
-        </div>
-      );
+    } else { // logged in
+      const { profile } = this.props;
+
+      if (profile.role === 'admin') {
+        return(
+          <div className='sidebar-menu'>
+            <strong className='sidebar-menu__heading'>Cool Guy</strong>
+            <ul className='sidebar-menu__list'>
+              <li className='sidebar-menu__item'><Link to="/">Home</Link></li>
+              <li className='sidebar-menu__item'><Link to="/addmovie">Add movie</Link></li>
+              <li className='sidebar-menu__item'><Link to="/schedule">Schedule</Link></li>
+              <li className='sidebar-menu__item'><a href="#" onClick={this.logout}>Logout</a></li>
+            </ul>
+            <hr />
+          </div>
+        );
+      } else if (profile.role === 'member') {
+        return(
+          <div className='sidebar-menu'>
+            <strong className='sidebar-menu__heading'>Cool Guy</strong>
+            <ul className='sidebar-menu__list'>
+              <li className='sidebar-menu__item'><Link to="/">Home</Link></li>
+              <li className='sidebar-menu__item'><a href="#" onClick={this.logout}>Logout</a></li>
+            </ul>
+            <hr />
+          </div>
+        );
+      } else { // unknown role?
+        return (<p>Who is this?</p>);
+      }
     }
   }
 }
+
+const wrappedLogin = firebaseConnect()(Login);
+export default connect(({ firebase }) => ({
+  auth: pathToJS(firebase, 'auth'),
+  profile: pathToJS(firebase, 'profile')
+}))(wrappedLogin);
