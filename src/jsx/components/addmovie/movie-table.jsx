5cb6f727a6964eac834d06cd6c70e707aaed0a6b
Sarah Berenji <sarahba@kth.se>
2017-04-12 14:10:50 +0200

    handle search if no movie is found

--- src/jsx/components/addmovie/movie-table.jsx
+++ src/jsx/components/addmovie/movie-table.jsx
@@ -6,9 +6,13 @@ export class MovieTable extends Component{
   render(){
     var rows = [];
     if(this.props.movies){
-      this.props.movies.forEach( movie => {
-        rows.push( <MovieRow {...movie} key={movie.id} />)
-      });
+      if(this.props.movies.length == 0){
+        rows = 'No movie was found'
+      } else {      
+        this.props.movies.forEach( movie => {
+          rows.push( <MovieRow {...movie} key={movie.id} />)
+        });
+      }
     }
 
   	return(
