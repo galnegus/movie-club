d474cd7327e7f7d0f1dc605649d98b9079ee61b3
Sarah Berenji <sarahba@kth.se>
2017-04-12 16:57:59 +0200

    Adding a movie to DB

--- src/jsx/components/addmovie/movie-row.jsx
+++ src/jsx/components/addmovie/movie-row.jsx
@@ -10,6 +10,7 @@ class MovieRow extends Component {
     super(props);
 
     this.addToFirebase = this.addToFirebase.bind(this);
+    this.findNextYearWeek = this.findNextYearWeek.bind(this);
   }
 
   // the datestring is formatted like YYYY-MM-DD and we only want YYYY.
@@ -17,17 +18,37 @@ class MovieRow extends Component {
     return datestring.slice(0, 4);
   }
 
-  findNextYearWeek() {
+  // checkIfMovieExists(movieID){
+  //   const { movies } = this.props;
+  //   return Object.keys(movies).map( key => movies[key]).some(movie => movie.api_data.id == movieID);
+  // }
 
+  findNextYearWeek() {
+    const { movies } = this.props;
+    // console.log("movies = ", movies)
+    // Object.keys(movies).map( key => (console.log("w = ", movies[key].year_week)))
+    const year_week = Object.keys(movies).map( key => movies[key].year_week);
+    // console.log("year_week = ", year_week)
+    const max = year_week.sort()[year_week.length-1];
+    let next;
+    // console.log("max = ", max)
+    // console.log("week = ", parseInt(max.substring(5)))
+    // console.log("year = ", parseInt(max.substring(0,4)))
+    if(parseInt(max.substring(5)) < 52){
+      next = max.substring(0,4) + "-"+ (parseInt(max.substring(5))+1).toString();
+      // console.log("next = ", next)
+    } else if(parseInt(max.substring(5)) == 52){
+      next = (parseInt(max.substring(0,4))+1).toString() + "-01";
+      // console.log("next = ", next)
+    }
+    return next;
   }
 
   // WIP
   addToFirebase(e) {
     e.preventDefault();
-
     // assume that the week will be stored in firebase a string, make it sortable in string format!
-    const current_year_week = moment().format('YYYY-ww'); // YYYY = 1970 1971 ... 2029 2030, ww = 01 02 ... 52 53
-
+    // const current_year_week = moment().format('YYYY-ww'); // YYYY = 1970 1971 ... 2029 2030, ww = 01 02 ... 52 53
     axios({
       method: 'get',
       url: 'https://api.themoviedb.org/3/movie/' + this.props.id,
@@ -37,14 +58,23 @@ class MovieRow extends Component {
       }
     }).then(response => {
       const { api_data } = response;
-      const movieObj = {year_week: current_year_week, api_data: response};
+      const nextYearWeek = this.findNextYearWeek();
+      // console.log("nextYearWeek = ", nextYearWeek)      
+      const movieObj = {year_week: nextYearWeek, api_data: response.data}; //, year: current_year, week: current_week};
+      this.props.firebase.push('/movies', movieObj);
     });
   }
 
+
   render() {
     // see https://developers.themoviedb.org/3/search/search-movies (response schema.results) to see what props are available
-
     const src = "http://image.tmdb.org/t/p/w92" + this.props.poster_path;
+    let addButton;
+    if (this.props.addedMoviesID.indexOf(this.props.id) > -1){
+      addButton = <p><span className='typcn typcn-tick' /></p>
+    } else {
+      addButton = <a href='#' className='add-movie-results__button' onClick={this.addToFirebase}><span className='typcn typcn-plus' /></a>
+    }
     return(
       <li className='add-movie-results__item'>
         {!this.props.poster_path ? '' : (<img className='add-movie-results__image' src={src}></img>)}
@@ -53,7 +83,7 @@ class MovieRow extends Component {
             <strong className='add-movie-results__title'>{this.props.title + ' (' + this.datestring2year(this.props.release_date) + ')'}</strong>
             <p className='add-movie-results__text'>{this.props.overview}</p>
           </div>
-          <a href='#' className='add-movie-results__button' onClick={this.addToFirebase}><span className='typcn typcn-plus' /></a>
+          {addButton}
         </div>
       </li>
     );
--- src/jsx/components/addmovie/movie-table.jsx
+++ src/jsx/components/addmovie/movie-table.jsx
@@ -1,16 +1,31 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import MovieRow from './movie-row.jsx';
+import { firebaseConnect, isLoaded, isEmpty, dataToJS } from 'react-redux-firebase';
+import { connect } from 'react-redux';
 
-export class MovieTable extends Component{
+class MovieTable extends Component{
+  constructor(props) {
+    super(props);
+    this.getAddedMoviesID = this.getAddedMoviesID.bind(this);
+  }
+
+  getAddedMoviesID(){
+    const { movies } = this.props;
+    if(isLoaded(movies)){
+      return Object.keys(movies).map( key => movies[key].api_data.id);
+    }
+  }
+
   render(){
+    const addedMoviesID = this.getAddedMoviesID();
     var rows = [];
-    if(this.props.movies){
-      if(this.props.movies.length == 0){
+    if(this.props.searchResults){
+      if(this.props.searchResults.length == 0){
         rows = 'No movie was found'
       } else {      
-        this.props.movies.forEach( movie => {
-          rows.push( <MovieRow {...movie} key={movie.id} />)
+        this.props.searchResults.forEach( movie => {
+          rows.push( <MovieRow {...movie} key={movie.id} addedMoviesID={addedMoviesID} />)
         });
       }
     }
@@ -22,3 +37,10 @@ export class MovieTable extends Component{
   	);
   }
 }
+
+const wrappedMovieTable = firebaseConnect([
+  { path: '/movies', queryParams: [ 'orderByChild=year_week' ] }
+])(MovieTable);
+export default connect(({ firebase }) => ({
+  movies: dataToJS(firebase, 'movies'),
+}))(wrappedMovieTable);
