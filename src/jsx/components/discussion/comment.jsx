3c447e76ae13489f1aaca29b6be46f3b5f33477a
galnegus <tobiashindersson@hotmail.com>
2017-04-17 02:32:49 +0200

    some more design tweaks

--- src/jsx/components/discussion/comment.jsx
+++ src/jsx/components/discussion/comment.jsx
@@ -2,12 +2,21 @@ import React, {Component} from 'react';
 import moment from 'moment';
 
 export default class Comment extends Component {
+
+  // make those newlines visible
+  // readmore: https://medium.com/@kevinsimper/react-newline-to-break-nl2br-a1c240ba746
+  nl2br(str) {
+    return str.split(/\r\n|\r|\n/).map((currentValue, index) => {
+      return <span key={index}>{currentValue}<br /></span>
+    });
+  }
+
   render() {
     return (
       <div className='comment'>
         <strong className='comment__author'>{this.props.commentData.author}</strong>
         <span className='comment__date'>{moment(this.props.commentData.date).calendar()}</span>
-        <p className='comment__text'>{this.props.commentData.text}</p>
+        <p className='comment__text'>{this.nl2br(this.props.commentData.text)}</p>
       </div>
     );
   }
