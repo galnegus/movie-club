bd0f9e26c7e54cbcac63e52c44d6767a646d236e
Sarah Berenji <sarahba@kth.se>
2017-04-18 20:16:04 +0200

    Fetching movie info from DB

--- src/jsx/components/discussion/MovieInfo.jsx
+++ src/jsx/components/discussion/MovieInfo.jsx
@@ -10,16 +10,30 @@ export default class MovieInfo extends Component {
 
   render() {
     const img_src = "http://image.tmdb.org/t/p/w92" + this.props.api_data.poster_path;
-    console.dir(this.props);
+    let directors=[], stars=[], genres=[];
+    const casts = this.props.api_data.credits.cast;
+    const crew = this.props.api_data.credits.crew;
+    const genre_list = this.props.api_data.genres;
+    if(crew) {
+      directors = crew.filter( d => d.job === "Director").map( d => d.name + ", ");
+      directors[directors.length-1] =  directors[directors.length-1].slice(0, -2);
+    }
+    if(casts && casts[0]) { stars.push(casts[0].name) };
+    if(casts && casts[1]) { stars.push(", "); stars.push(casts[1].name); }
+    if(genre_list) {
+      genres = genre_list.map( g => g.name + ", ");
+      genres[genres.length-1] =  genres[genres.length-1].slice(0, -2);
+    }
 
+    // console.log("genres = ", genres)
     return (
       <div className='movie-info'>
         <img className='movie-info__poster' src={img_src} />
         <div className='movie-info__content'>
           <h3>{this.props.api_data.title} <small className='color-grey'>({this.datestring2year(this.props.api_data.release_date)})</small></h3>
-          <p className='color-grey'>1h 48m | Horror, Comedy, Drama</p>
-          <p><strong>Director:</strong> John Fawcett</p>
-          <p><strong>Stars:</strong> Katharine Isabelle, Emily Perkins</p>
+          <p className='color-grey'>{this.props.api_data.runtime} minutes  | {genres}</p>
+          <p><strong>Director(s):</strong> {directors} </p>
+          {stars.length>0 ? (<p><strong>Stars:</strong> {stars}</p>) : ""}
           <p>{this.props.api_data.overview}</p>
         </div>
       </div>
